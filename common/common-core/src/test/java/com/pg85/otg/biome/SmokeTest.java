df4d5e7c4b4a5628223d42120d2292e345eed3a9
JMc <jkmendyka@gmail.com>
2026-02-16 23:05:52 +0100

    chore: add JUnit 5 test infrastructure to common-core

---
+++ common/common-core/src/test/java/com/pg85/otg/biome/SmokeTest.java
@@ -0,0 +1,11 @@
+package com.pg85.otg.biome;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SmokeTest {
+    @Test
+    void junitWorks() {
+        assertTrue(true);
+    }
+}
