72872e8d21dab6d5b262158efaeb43295df316f3
JMc <jkmendyka@gmail.com>
2026-03-29 23:24:35 +0200

    fix(editor): move OTG Editor button lower to avoid Options/Quit overlap

--- platforms/shared/src/client/java/com/pg85/otg/client/mixin/TitleScreenMixin.java
+++ platforms/shared/src/client/java/com/pg85/otg/client/mixin/TitleScreenMixin.java
@@ -23,6 +23,6 @@ public abstract class TitleScreenMixin extends Screen {
         addRenderableWidget(Button.builder(
             Component.literal("OTG Editor"),
             btn -> minecraft.setScreen(new EditorHubScreen())
-        ).bounds(width / 2 - 50, height / 4 + 156, 100, 20).build());
+        ).bounds(width / 2 - 50, height / 4 + 168, 100, 20).build());
     }
 }
